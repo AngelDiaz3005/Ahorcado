28cea637998a885799d3bce75a6124db71c7c6a3
AngelDiaz <dariodiaz.3005@gmail.com>
2019-10-04 21:48:05 -0500

    ahorcado final

--- Ahorcado.cpp
+++ Ahorcado.cpp
@@ -3,7 +3,25 @@
 
 using namespace std;
 
+int sizechar(char* palabra) {
+	int cont = 0;
+	for (char* p = palabra; *p != '\0'; p++) {
+		cont++;
+	}
+	return cont;
+}
 
+void errorcont(char* word, char* letra, int* error, int size) {
+	int aux = 0;
+	for (char* p = word; p < word + size; p++) {
+		if (*p != *letra) {
+			aux++;
+		}
+	}
+	int aux2 = aux / size;
+
+	*error += aux2;
+}
 
 void busqueda(char *palabra, char *letra, char* palabravacia) {
 	char* p1 = palabra;
@@ -12,7 +30,7 @@ void busqueda(char *palabra, char *letra, char* palabravacia) {
 	int cont = 0;
 	for (char* p = p1 ; *p != '\0' ; p++, cont++) {
 		if (*p == *p2) {
-			*(p3+cont)=*p2;
+			*(p3 + cont) = *p2;
 		}
 	}
 }
@@ -23,61 +41,173 @@ void imprimir(char* ini, char* final) {
 	}
 }
 
-int sizechar( char* palabra) {
-	int cont = 0;
-	for (char* p = palabra; *p != '\0'; p++) {
-		cont++;
+void comparar(int size, int* p, char* word1, char* word2) {
+	int aux = 0;
+	int i = 0;
+	char* p2 = word2;
+	for (char* l = word1; l < word1 + size; l++ ) {
+		if (*l == *(p2 + aux)) {
+			i++;
+		}
+		aux++;
 	}
-	return cont;
+	if (i == size) {
+		*p = 0;
+	}
+
+}
+
+void imppalabra(char* palabra) {
+	for (char* p = palabra; *p !='\0'; p++) {
+		cout << *p;
+	}
 }
 
 int main() {
 
 	char palabra [100];
 	int cont = 0;
-	int i = 0;
+	int error = 0;
 	int continuar = 1;
 
 	cout << "Ingrese palabra para el ahorcado: "; cin >> palabra; cout << endl;
+
 	
+	system("cls");
+
 	int const size = sizechar(palabra);
 
-	char* palabra2 = new char[size];
 	char* palabravacia = new char[size];
+	char* letra = new char[1];
 
-	for (char* p = palabra; *p != '\0'; p++) {
-		*(palabra2+i) = *p;
-		i++;
-	}
 
-	while (continuar) {
+	cout << endl;
+	while (continuar == 1) {
 
-		char letra[1];
+		if (error == 0) {
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |			" << endl;
+			cout << "___________|___________" << endl;
+		}
+		if (error == 1) {
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |          (   )" << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |			" << endl;
+			cout << "___________|___________" << endl;
+		}
+		if (error == 2) {
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |          (   )" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |			" << endl;
+			cout << "___________|___________" << endl;
+		}
+		if (error == 3) {
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |          (   )" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |          :'|" << endl;
+			cout << "           |         :' |" << endl;
+			cout << "           |        :'  |" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |			" << endl;
+			cout << "___________|___________" << endl;
+		}
+		if (error == 4) {
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |          (   )" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |          :'|':" << endl;
+			cout << "           |         :' | ':" << endl;
+			cout << "           |        :'  |  ':" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |           " << endl;
+			cout << "           |           " << endl;
+			cout << "           |			" << endl;
+			cout << "___________|___________" << endl;
+		}
+		if (error == 5) {
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |          (   )" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |          :'|':" << endl;
+			cout << "           |         :' | ':" << endl;
+			cout << "           |        :'  |  ':" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |          :'" << endl;
+			cout << "           |         :' " << endl;
+			cout << "           |        :'  " << endl;
+			cout << "___________|___________ " << endl;
+		}
+		if (error == 6) {
+			cout << "PERDISTE" << endl;
+			cout << "La palabra era: "; imppalabra(palabra); cout << endl;
+			cout << "           ______________ " << endl;
+			cout << "           |            | " << endl;
+			cout << "           |          (x x)" << endl;
+			cout << "           |         --------" << endl;
+			cout << "           |          :'|':" << endl;
+			cout << "           |         :' | ':" << endl;
+			cout << "           |        :'  |  ':" << endl;
+			cout << "           |            |" << endl;
+			cout << "           |          :' ':" << endl;
+			cout << "           |         :'   ':" << endl;
+			cout << "           |        :'     ':" << endl;
+			cout << "___________|__________________" << endl;
+			exit(1);
+		}
+		cout << endl;
 
 		cout << "Ingrese letra :"; cin >> letra;
 
+
 		busqueda(palabra, letra, palabravacia);
+		
+		comparar(size, &continuar, palabra, palabravacia);
+
+		errorcont(palabra, letra, &error, size);
+
+		cout << endl;
+		
+		system("cls");
 
 		imprimir(palabravacia, palabravacia + size);
-		
-		if (strcmp(palabra2, palabravacia)==0) {
-			continuar = 0;
-		};
-
 		cout << endl;
 
 		
-
-	//	for (char* p = &palabra; palabra != '\0'; p++, i++) {
-	//		if(*p==*(palabravacia + i)){
-	//			cont++;
-	//		}
-	//		if (cont == size) {
-	//			continuar = 0;
-	//		}
-	//	}
-	//	cont = 0;
 	}
+	cout << "GANASTE!!";
+
+	exit(1);
 
 	return 0;
 }
